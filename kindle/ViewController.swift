ca259511a31b3d0a3f259685fa640e67c4a0ab7e
marvin evins <maevins@cps.edu>
2017-05-15 18:25:17 -0500

    added cell configuration

--- kindle/ViewController.swift
+++ kindle/ViewController.swift
@@ -15,11 +15,37 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.tableFooterView = UIView()
+        
         navigationItem.title = "Kindle"
-        view.backgroundColor = .red
+       // view.backgroundColor = .red
         // can provide custom code starting here
+        
+        
+        
         setUpBooks()
         
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+       let book =  books?[indexPath.row]
+        
+        
+        cell.textLabel?.text = book?.title
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let count = books?.count{
+            return count
+            
+        }
+        return  0
     }
     
     
@@ -51,36 +77,7 @@ class ViewController: UITableViewController {
         
         self.books = [book, book2]
         
-        guard let books = self.books else{
-            return
-        }
-        
-        //        for book in books{
-        //
-        //            print(book.title)
-        //            for page in book.pages{
-        //                print(page.text)
-        //
-        //            }
-        //
-        //        }
-        
-        
-        
-        
-        if let unwrappedBooks = self.books{
-            for book in unwrappedBooks{
-                
-                print(book.title)
-                for page in book.pages{
-                    print(page.text)
-                    
-                }
-                
-            }
-            
-            
-        }
+       
         
     
 
