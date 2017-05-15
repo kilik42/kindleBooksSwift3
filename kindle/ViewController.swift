3ccb0c7fc1130877f77195001b66be75cd949785
marvin evins <maevins@cps.edu>
2017-05-15 17:37:45 -0500

    created an array for books object and refactored whole procedure into separate function

--- kindle/ViewController.swift
+++ kindle/ViewController.swift
@@ -33,60 +33,80 @@ class Page {
 }
 
 class ViewController: UIViewController {
+    var books: [Book]?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
         // can provide custom code starting here
+        setUpBooks()
         
-        
+    }
+    
+    
+    
+    func setUpBooks(){
         let page1 = Page(number: 1, text: "text is for the 1st page")
         let page2 = Page(number: 2, text: "text for second page")
         
-       
+        
         
         let pages = [page1, page2]
         
         let book = Book (title: "STeve jobs", author: "walter syen", pages: pages)
         
         print(book.title, book.author)
-
+        
         print("Title: \(book.title), Author: \(book.author)")
         
-//        let firstPage = book.pages[0]
-//        
-//        if page2.number == 2 {
-//             print(page2.text)
-//            
-//        }
-        
-//        for page in book.pages{
-//            if page.number == 1 {
-//            print(page.text)
-//        }
         
         
-            let book2 = Book(title: "bill gates a bio", author: "michael boc", pages: [
-                Page(number: 1, text: "text for page 1"),
-                Page(number: 2, text: "text for page 2"),
-                Page(number: 3, text: "text for page 3"),
-                Page(number: 4, text: "text for page 4")
-                
-                ])
         
-        for book in [book, book2]{
-    
-            print(book.title)
-            for page in book.pages{
-                print(page.text)
-    
-                }
-    
+        let book2 = Book(title: "bill gates a bio", author: "michael boc", pages: [
+            Page(number: 1, text: "text for page 1"),
+            Page(number: 2, text: "text for page 2"),
+            Page(number: 3, text: "text for page 3"),
+            Page(number: 4, text: "text for page 4")
+            
+            ])
+        
+        self.books = [book, book2]
+        
+        guard let books = self.books else{
+            return
         }
+        
+        //        for book in books{
+        //
+        //            print(book.title)
+        //            for page in book.pages{
+        //                print(page.text)
+        //
+        //            }
+        //
+        //        }
+        
+        
+        
+        
+        if let unwrappedBooks = self.books{
+            for book in unwrappedBooks{
+                
+                print(book.title)
+                for page in book.pages{
+                    print(page.text)
+                    
+                }
+                
+            }
             
             
-    }
+        }
+        
+    
 
+    }
     
 }
 
